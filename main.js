f69a8fdbf1bbcdd442e2bbc9f2e6c0e1503eb742
pozdnyakoks <pozdnyakoks@mail.ru>
2023-06-14 15:57:21 -0300

    add functionality to links

--- main.js
+++ main.js
@@ -30,7 +30,6 @@ document.addEventListener('click', (ev) => {
 
 function reveal() {
   const animate = document.querySelectorAll('.animate')
-
   for (let i = 0; i < animate.length; i++) {
     let windowHeight = window.innerHeight;
     let elementTop = animate[i].getBoundingClientRect().top;
@@ -56,8 +55,79 @@ document.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
     heroTitle.classList.add('scroll')
     heroImg.classList.add('scroll')
-  }, 1000)
+  }, 2000)
 })
 
+
+const links = document.querySelectorAll('a');
+links.forEach(el => {
+  if (el.getAttribute('href')[0] === '/') {
+    el.addEventListener('click', (ev) => {
+      ev.preventDefault()
+
+      const prevPath = window.location.pathname;
+      history.pushState({}, 'link', el.getAttribute('href'));
+      changePath(prevPath, window.location.pathname)
+    })
+  }
+})
+
+function changePath(prevPath, path) {
+  const home = document.getElementById('home');
+  const selector = path.slice(1);
+  const prevSelector = prevPath.slice(1);
+
+  if (path === '/' && prevPath === '/') {
+    home.classList.add('block', 'visible');
+  }
+
+  else if (path === prevPath) {
+    document.getElementById(selector).classList.add('block', 'visible');
+  }
+
+  else if (path !== prevPath && prevPath === '/') {
+    home.classList.remove('visible');
+
+    setTimeout(() => {
+      home.classList.remove('block')
+      document.getElementById(selector).classList.add('block');
+      document.getElementById(selector).classList.add('visible')
+    }, 600)
+  }
+
+  else if (path !== prevPath && path === '/') {
+    document.getElementById(prevSelector).classList.remove('visible');
+    setTimeout(() => {
+      home.classList.add('block')
+      document.getElementById(prevSelector).classList.remove('block');
+      home.classList.add('visible')
+    }, 600)
+  }
+
+  else {
+    document.getElementById(prevSelector).classList.remove('visible');
+    // setTimeout(() => {
+    document.getElementById(prevSelector).addEventListener('transitionend', () => {
+      document.getElementById(prevSelector).classList.remove('block')
+      document.getElementById(selector).classList.add('block');
+      document.getElementById(selector).classList.add('visible')
+
+    })
+    // }, 600)
+
+  }
+
+  // header links
+  const headerLinks = document.querySelectorAll('.header-nav-list-item-link');
+  headerLinks.forEach(link => {
+    if (link.getAttribute('href') === path) {
+      link.classList.add('active')
+    } else {
+      link.classList.remove('active')
+    }
+  })
+}
+
+changePath(window.location.pathname, window.location.pathname)
 
 
