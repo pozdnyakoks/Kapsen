830549d5a662460db3c1577b0d221c5a674a49fb
pozdnyakoks <pozdnyakoks@mail.ru>
2023-06-16 10:01:15 -0300

    remove comment in vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default {
-  // plugins: [
-  //   viteStaticCopy({
-  //     targets: [
-  //       {
-  //         src: './src/assets/images/Tyres',
-  //         dest: 'assets'
-  //       }
-  //     ]
-  //   })
-  // ]
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: './src/assets/images/Tyres',
+          dest: 'assets'
+        }
+      ]
+    })
+  ]
 }
